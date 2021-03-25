2d57102d0f071948d24acf20674aed4285479b0a
ulrichbenedikt <ulrichbenedikt@gmail.com>
2021-03-25 12:18:50 +0100

    update

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -2,7 +2,7 @@
   <v-card v-if="user" class="pa-4"
     ><v-card-title
       ><v-row
-        ><v-col cols="3"
+        ><v-col cols="4"
           ><v-avatar color="primary" size="56"
             ><v-img
               :lazy-src="user.profilImg"
@@ -10,13 +10,95 @@
               max-width="250"
               :src="user.profilImg"
             ></v-img></v-avatar></v-col
-        ><v-col cols="9"
+        ><v-col cols="8" class="pl-2"
           ><div>
-            <h1 class="text-h6">{{ user.firstname }} {{ user.lastname }}</h1>
+            <h1 class="text-h6" style="line-height: 1.1em">
+              {{ user.firstname }}<br />{{ user.lastname }}
+            </h1>
           </div>
           <p class="caption">{{ user.membership }}</p></v-col
         ></v-row
       ></v-card-title
+    ><v-divider></v-divider
+    ><v-card-subtitle
+      ><v-row>
+        <v-col cols="6" style="line-height: 10px"
+          ><v-btn plain class="caption pa-0" small height="20">eMail me</v-btn
+          ><v-btn plain class="caption pa-0" small height="20"
+            >Follow me</v-btn
+          ></v-col
+        ><v-col cols="6" style="line-height: 5px"
+          ><p class="caption ma-0 text-right">
+            <b
+              ><span class="primary--text">{{ user.followers }}</span>
+              Followers</b
+            >
+          </p>
+          <p class="caption ma-0 text-right">
+            <b
+              ><span class="primary--text">{{ user.views }}</span> Views</b
+            >
+          </p>
+          <p class="caption ma-0 text-right">
+            <b
+              ><span class="primary--text">{{ user.likes }}</span> Likes</b
+            >
+          </p></v-col
+        >
+      </v-row></v-card-subtitle
+    ><v-card-text>
+      <p>{{ user.shortDescription }}</p>
+      <p class="caption mb-0 mt-4">Main Subjects</p>
+      <v-divider></v-divider>
+      <div class="text-center" :key="key" v-for="key in user.mainSubjects">
+        <v-chip class="ma-2" x-small>
+          {{ key }}
+        </v-chip>
+      </div>
+      <p v-if="user.mainSubjects.length === 0" class="caption">no items</p>
+      <p class="caption mb-0 mt-4">Main Styles</p>
+      <v-divider></v-divider>
+      <div class="text-center" :key="key" v-for="key in user.mainStyles">
+        <v-chip class="ma-2" x-small>
+          {{ key }}
+        </v-chip>
+      </div>
+      <p v-if="user.mainStyles.length === 0" class="caption">no items</p>
+      <p class="caption mb-0 mt-4">Key words</p>
+      <v-divider></v-divider>
+      <div class="text-center" :key="key" v-for="key in user.keyWords">
+        <v-chip class="ma-2" x-small>
+          {{ key }}
+        </v-chip>
+      </div>
+      <p v-if="user.keyWords.length === 0" class="caption">no items</p>
+    </v-card-text>
+    <v-card-actions>
+      <v-row>
+        <v-col cols="12" sm="3">
+          <v-btn icon>
+            <v-icon>mdi-facebook</v-icon>
+          </v-btn>
+        </v-col>
+
+        <v-col cols="12" sm="3">
+          <v-btn icon>
+            <v-icon>mdi-twitter</v-icon>
+          </v-btn>
+        </v-col>
+
+        <v-col cols="12" sm="3">
+          <v-btn icon>
+            <v-icon>mdi-pinterest</v-icon>
+          </v-btn>
+        </v-col>
+
+        <v-col cols="12" sm="3">
+          <v-btn icon>
+            <v-icon>mdi-instagram</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row></v-card-actions
     ></v-card
   >
 </template>
---
+++ pages/profil/index.vue
@@ -0,0 +1,102 @@
+<template>
+  <v-container>
+    <h1 class="text-h1">Dashboard</h1>
+    <v-subheader>What to do</v-subheader>
+    <v-row>
+      <v-col cols="4">
+        <v-card class="mx-auto" max-width="344">
+          <v-img src="/lory-Vei-eKMKR40-unsplash.jpg" height="150px"></v-img>
+
+          <v-card-title> Upload Photos </v-card-title>
+
+          <v-card-subtitle>
+            Upload your Photos and create Collections or Rolls.</v-card-subtitle
+          >
+
+          <v-card-actions>
+            <v-btn color="primary" text> Continue </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="4">
+        <v-card class="mx-auto" max-width="344">
+          <v-img
+            src="/norbert-kundrak-LJcB7n2F3UQ-unsplash.jpg"
+            height="150px"
+          ></v-img>
+
+          <v-card-title> Change Settings </v-card-title>
+
+          <v-card-subtitle>
+            Overview of your money and your profile settings.</v-card-subtitle
+          >
+
+          <v-card-actions>
+            <v-btn color="primary" text> Continue </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="4">
+        <v-card class="mx-auto" max-width="344">
+          <v-img
+            src="/clem-onojeghuo-xJXxMR5PXoY-unsplash.jpg"
+            height="150px"
+          ></v-img>
+
+          <v-card-title> Search Photos </v-card-title>
+
+          <v-card-subtitle>
+            Browse through photos, collections and rolls of our community.
+          </v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn color="primary" text> Continue </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-subheader>Statistics</v-subheader>
+    <v-row
+      ><v-col>
+        <v-card>
+          <v-sparkline
+            :value="value"
+            :smooth="radius || false"
+            :padding="padding"
+            :line-width="width"
+            :stroke-linecap="lineCap"
+            :fill="fill"
+            :type="type"
+            :auto-line-width="autoLineWidth"
+            auto-draw
+          >
+            <template v-slot:label="item"> ${{ item.value }} </template>
+          </v-sparkline>
+          <v-container class="pt-0">
+            <v-divider></v-divider>
+            <div class="px-2 d-flex justify-space-between">
+              <p class="caption">Jan. 2020</p>
+              <p class="caption">Jan. 2021</p>
+            </div>
+            <v-subheader class="px-2">Your earnings this year</v-subheader>
+          </v-container>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    width: 1.3,
+    radius: 50,
+    padding: 8,
+    lineCap: 'round',
+    value: [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0],
+    fill: false,
+    type: 'trend',
+    autoLineWidth: false,
+  }),
+}
+</script>
